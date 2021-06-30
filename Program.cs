b1a706b3eece72a2e115aebaa029784ab55526f0
Paulo Garcia <paulogarcia830@gmail.com>
2021-06-30 14:24:50 -0300

    Initiall

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Desafio_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
