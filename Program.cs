fad4a07c3422a4ed28c3821f14d21feeb7a11769
Eliel Contractor <eliel.contractor@am.jll.com>
2021-06-30 23:26:17 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,51 @@ namespace Desafio_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Imprime na tela
+            Console.WriteLine("UBUNTU 002 - STARTS...");
+
+            string nome1 = "Ubuntu1 Silva";
+            int idade1   = 33;
+            double peso1 = 88.50;
+            decimal altura1 = 1.65M;
+            bool EhDev1  = true;
+
+            string nome2 = "Ubuntu2 Santos";
+            int idade2 = 26;
+            double peso2 = 84.50;
+            decimal altura2 = 2.10M;
+            bool EhDev2 = true;
+
+            string nome3 = "Ubuntu3 Silva";
+            int idade3 = 85;
+            double peso3 = 66.50;
+            decimal altura3 = 1.40M;
+            bool EhDev3 = false;
+
+
+            //T1 - Somar a idade de todos Ubuntus
+
+            //T2 - Agrupar os nomes de todos Ubuntus
+
+            //T3 - Calcular Média dos IMC de todos Ubuntus
+
+            //T4 - Contar quantos Ubuntus sao Devs
+
+            //T5 - Exibir somente Ubuntu com nome Silva
+
+
+
+
+
+
+            //Imprime na tela
+            Console.WriteLine("UBUNTU 002 - ENDS...");
+
+            //PAUSA NA TELA (GAMBIARRA PERMITIDA PRA INICIANTES)
+            Console.Read();
         }
+
+
     }
 }
