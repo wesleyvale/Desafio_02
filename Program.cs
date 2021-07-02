c8a3e8e31c29724ce50c7c2cfc3989dbe92ed0a4
Vilela <victor_vilela@live.com>
2021-07-02 10:54:04 -0300

    Well Done!

--- Program.cs
+++ Program.cs
@@ -30,16 +30,85 @@ namespace Desafio_02
 
 
             //T1 - Somar a idade de todos Ubuntus
+            int IDADE = idade1 + idade2 + idade3;
+            Console.WriteLine($"Os Ubuntus possuem uma idade total de: {IDADE}.");
+
 
             //T2 - Agrupar os nomes de todos Ubuntus
+            Console.WriteLine($"Os Ubuntus presentes são: {nome1}, {nome2} e {nome3}");
+
 
             //T3 - Calcular Média dos IMC de todos Ubuntus
+            decimal IMC1 = (decimal)peso1 / (altura1 * altura1);
+            decimal IMC2 = (decimal)peso2 / (altura2 * altura2);
+            decimal IMC3 = (decimal)peso3 / (altura3 * altura3);
+            decimal media = (IMC1 + IMC2 + IMC3) / 3;
+            Console.WriteLine($"A média de IMC dos Ubuntus resulta em: {media.ToString("f")}.");
+
 
             //T4 - Contar quantos Ubuntus sao Devs
+            int EhDev = 0;
+
+            if (EhDev1 == true)
+            {
+                EhDev = EhDev+1;
+            }
+            else
+            {
+                EhDev = EhDev;
+            };
+            if (EhDev2 == true)
+            {
+                EhDev = EhDev + 1;
+            }
+            else
+            {
+                EhDev = EhDev;
+            };
+            if (EhDev3 == true)
+            {
+                EhDev = EhDev + 1;
+            }
+            else
+            {
+                EhDev = EhDev;
+            };
+            Console.WriteLine($"Dentre os Ubuntos,{EhDev} são Devs!");
+
 
             //T5 - Exibir somente Ubuntu com nome Silva
+            bool Silva1 = nome1.Contains("Silva");
+            bool Silva2 = nome2.Contains("Silva");
+            bool Silva3 = nome3.Contains("Silva");
+            string Silva = "";
+
+            if (Silva1 == true)
+            {
+                Silva = Silva + $" {nome1},";
+            }
+            else
+            {
+                Silva = Silva;
+            };
+            if (Silva2 == true)
+            {
+                Silva = Silva + $" {nome2},";
+            }
+            else
+            {
+                Silva = Silva;
+            };
+            if (Silva3 == true)
+            {
+                Silva = Silva + $" {nome3},";
+            }
+            else
+            {
+                Silva = Silva;
+            };
 
 
+            Console.WriteLine($"Dentre os Ubuntos:{Silva} possuem Silva no nome.");
 
             //Imprime na tela
             Console.WriteLine("UBUNTU 002 - ENDS...");
