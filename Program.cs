ec9a2e5912f8fa9af590eea43efa1365de7a7dfa
Mayara <git.qodeless@outlook.com>
2021-07-03 00:37:15 -0300

    T1: Imprimir na tela a soma das idades de todos os Ubuntus;
    T2: Realizar a média dos IMC's de todos Ubuntus;
    T3: Imprimir na tela quantos Ubuntus são Dev;
    T4: Imprimir na tela quais Ubuntus possuem o nome "Silva".

--- Program.cs
+++ Program.cs
@@ -4,50 +4,77 @@ namespace Desafio_02
 {
     class Program
     {
+        static string[] nomes = new string[3] { "Ubuntu1 Silva", "Ubuntu2 Santos", "Ubuntu3 Silva" };
+        static int[] idades = new int[3] { 33, 26, 85 };
+        static double[] pesos = new double[3] { 88.50, 84.50, 66.50 };
+        static decimal[] alturas = new decimal[3] { 1.65M, 2.10M, 1.40M };
+        static bool[] EhDev = new bool[3] { true, true, false };
+
         static void Main(string[] args)
         {
+            int somaIdades = 0;
 
-            //Imprime na tela
-            Console.WriteLine("UBUNTU 002 - STARTS...");
+            foreach(int idade in idades)
+            {
+                somaIdades += idade;
+            }
 
-            string nome1 = "Ubuntu1 Silva";
-            int idade1   = 33;
-            double peso1 = 88.50;
-            decimal altura1 = 1.65M;
-            bool EhDev1  = true;
+            Console.WriteLine("T1: A soma das idades de todos os Ubuntus é: " + somaIdades + " anos");
 
-            string nome2 = "Ubuntu2 Santos";
-            int idade2 = 26;
-            double peso2 = 84.50;
-            decimal altura2 = 2.10M;
-            bool EhDev2 = true;
+            NomesUbuntu();
 
-            string nome3 = "Ubuntu3 Silva";
-            int idade3 = 85;
-            double peso3 = 66.50;
-            decimal altura3 = 1.40M;
-            bool EhDev3 = false;
+            MediaIMC();
 
+            UbuntusDev();
 
-            //T1 - Somar a idade de todos Ubuntus
+            UbuntusSilva();
 
-            //T2 - Agrupar os nomes de todos Ubuntus
-
-            //T3 - Calcular Média dos IMC de todos Ubuntus
-
-            //T4 - Contar quantos Ubuntus sao Devs
-
-            //T5 - Exibir somente Ubuntu com nome Silva
-
-
-
-            //Imprime na tela
-            Console.WriteLine("UBUNTU 002 - ENDS...");
-
-            //PAUSA NA TELA (GAMBIARRA PERMITIDA PRA INICIANTES)
-            Console.Read();
+            Console.ReadLine();
         }
+        
+        static void NomesUbuntu()
+        {
+            Console.Write("T2: O nome de todos os Ubuntus são: ");
 
+            foreach (string nome in nomes)
+            {
+                Console.Write(nome + " ");
+            }
+        }
+        static void MediaIMC()
+        {
+            double somaDosIMCS = 0;
 
+            for (int contador = 0; contador < pesos.Length; contador++)
+            {
+                somaDosIMCS += pesos[contador] / (decimal.ToDouble(alturas[contador]) * decimal.ToDouble(alturas[contador]));
+            }
+
+            Console.Write("\nT3: A média dos IMC'S de todos os Ubuntus é: " + Math.Round(somaDosIMCS / pesos.Length, 2));
+        }
+        static int UbuntusDev()
+        {
+            int soma = 0;
+
+            foreach(bool EhDev in EhDev)
+            {
+                if (EhDev == true) soma++;
+            }
+            Console.Write("\nT4: A quantidade de Ubuntus Dev é: " + soma);
+
+            return soma;
+        }
+        static void UbuntusSilva()
+        {
+            Console.Write("\nT5: Os Ubuntus com o nome 'Silva' são: ");
+
+            foreach(string nome in nomes)
+            {
+                 if (nome.Contains("Silva"))
+                 {
+                    Console.Write(nome + " ");
+                 }
+            }
+        }
     }
 }
