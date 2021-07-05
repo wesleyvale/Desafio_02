e8f342e53eefd953b534351694a412688b6811d2
jeffdev <jefferson-67@live.com>
2021-07-05 17:43:55 -0300

    desafio2

---
+++ desafio_2/desafio_2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace desafio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ubuntu;
+
+            string nome1 = "ubuntu1 Silva";
+            int idade1 = 33;
+            double peso1 = 88.50;
+            double altura1 = 1.65;
+            //bool EhDev1 = true;
+
+            string nome2 = "ubuntu2 Santos";
+            int idade2 = 26;
+            double peso2 = 84.50;
+            double altura2 = 2.10;
+            //bool EhDev2 = true;
+
+            string nome3 = "ubuntu3 Silva";
+            int idade3 = 85;
+            double peso3 = 66.50;
+            double altura3 = 1.40;
+            //bool EhDev3 = false;
+
+            Console.WriteLine("UBUNTU 002 - STARTS... \n");
+            Console.WriteLine("Tudo o que você sempre quis saber sobre alguns de nossos ubuntus\n");
+            Console.WriteLine("1- A soma da idade deles");
+            Console.WriteLine("2- Quem são esses Ubuntus?");
+            Console.WriteLine("3- O IMC de cada um deles");
+            Console.WriteLine("4- São todos devs?");
+            Console.WriteLine("5- Apenas os Silva\n");
+
+            {
+                Console.Write("Digite a opção que deseja\n");
+                ubuntu = (Console.ReadLine());
+                while (ubuntu != "0")
+                {
+                    switch (ubuntu)
+                    {
+                        case "1":
+                            Console.WriteLine("\nTodos juntos tem " + (idade1 + idade2 + idade3) + " anos de idade.\n");
+                            break;
+                        case "2":
+                            Console.WriteLine("\nNossos ubuntus são: \n" + nome1 + "\n" + nome2 + "\n" + nome3 + "\n");
+                            break;
+                        case "3":
+                            Console.WriteLine("\nIMC é do Ubuntu1 Silva: " + ((peso1 / (altura1 * altura1)).ToString("0.00")));
+                            Console.WriteLine("IMC é do Ubuntu2 Santos: " + ((peso2 / (altura2 * altura2)).ToString("0.00")));
+                            Console.WriteLine("IMC é do Ubuntu3 Silva: " + ((peso3 / (altura3 * altura3)).ToString("0.00\n")));
+                            break;
+                        case "4":
+                            try
+                            {
+                                Console.Write("\n");
+                                dev d = new dev();
+                                d.Contagem();
+                            }
+                            catch
+                            {
+                                return;
+                            }
+                            //if (EhDev1 == true && EhDev2 ==true)
+                            //{
+                            //    Console.WriteLine("\nUbuntu1 Silva e o Ubuntu2 Santos são devs.");
+                            //}
+
+                            //if (EhDev3 == true)
+                            //{
+                            //    Console.WriteLine("\nUbuntu3 Silva é um dev");
+                            //}
+                            //{
+                            //    Console.WriteLine("E o Ubuntu3 Silva não é um dev...ainda\n");
+                            //}
+                            break;
+                        case "5":
+                            Console.WriteLine("\nUbuntus que tem o sobrenome Silva: \n" + "Ubuntu1 Silva\n" + "Ubuntu3 Silva\n");
+                            break;
+
+                        default:
+                            Console.WriteLine("opção inválida!");
+                            break;
+
+                    }
+                    Console.WriteLine("\nDeseja saber mais?");
+                    Console.WriteLine("Digite 0 para sair ou uma outra opção para continuar");
+                    ubuntu = (Console.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ desafio_2/desafio_2/dev.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace desafio_2
+{
+    class dev
+    {
+        public string nome { get; set; }
+        public bool desenvolvedor { get; set; }
+
+        public void Contagem()
+        {
+            dev[] ubuntus =
+            {
+            new dev {nome = "Ubuntu1 Silva", desenvolvedor=true},
+            new dev{nome = "Ubuntu2 Santos", desenvolvedor=true},
+            new dev {nome = "Ubuntu3 Silva", desenvolvedor=false}
+        };
+
+            try
+            {
+                int ehDesenvolvedor = ubuntus.Count(u => u.desenvolvedor == true);
+                Console.WriteLine("Apenas {0} são Devs ", ehDesenvolvedor);
+            }
+            catch (OverflowException)
+            {
+                return;
+            }
+        }
+    }
+}
