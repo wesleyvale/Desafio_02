b6d713b1210d6cfb0a00f256070e97b775ad236f
bhoehne <rookie.qodeless@outlook.com>
2021-08-17 11:52:06 -0300

    Desafio 2 feito

--- Program.cs
+++ Program.cs
@@ -30,19 +30,66 @@ namespace Desafio_02
 
 
             //T1 - Somar a idade de todos Ubuntus
+            int idade4 = idade1 + idade2 + idade3;
 
             //T2 - Agrupar os nomes de todos Ubuntus
-
+            string nome4 = $"seus nomes são {nome1}, {nome2} e {nome3}";
             //T3 - Calcular Média dos IMC de todos Ubuntus
-
+            decimal sAltura = altura1+altura2+altura3;
+            decimal qAltura = sAltura*sAltura;
+            decimal dPeso1 = Convert.ToDecimal(peso1);
+            decimal dPeso2 = Convert.ToDecimal(peso2);
+            decimal dPeso3 = Convert.ToDecimal(peso3);
+            decimal sPeso = dPeso1+dPeso2+dPeso3;
+            decimal IMC = sPeso/qAltura;
             //T4 - Contar quantos Ubuntus sao Devs
+            int EhDev4 = 0;
+            if(EhDev1)
+            {
+                EhDev4 += 1;
+            } else
+            {
+                EhDev4 += 0;
+            }
 
+            if (EhDev2)
+            {
+                EhDev4 += 1;
+            }
+            else
+            {
+                EhDev4 += 0;
+            }
+
+            if (EhDev3)
+            {
+                EhDev4 += 1;
+            }
+            else
+            {
+                EhDev4 += 0;
+            }
             //T5 - Exibir somente Ubuntu com nome Silva
+            string nomeSilva4 = "e ";
+            bool nomeSilva1 = nome1.Contains("Silva");
+            bool nomeSilva2 = nome2.Contains("Silva");
+            bool nomeSilva3 = nome3.Contains("Silva");
 
-
+            if (nomeSilva1)
+            {
+                nomeSilva4 = nome1 + " e ";
+            }
+            if (nomeSilva2)
+            {
+                nomeSilva4 += nome2 + " e ";
+            }
+            if (nomeSilva3)
+            {
+                nomeSilva4 += nome3;
+            }
 
             //Imprime na tela
-            Console.WriteLine("UBUNTU 002 - ENDS...");
+            Console.WriteLine($"Os Ubuntus aqui tem a idade total de {idade4}, {nome4}, o IMC médio deles é {IMC}, {EhDev4} são devs {nomeSilva4} chamam Silva");
 
             //PAUSA NA TELA (GAMBIARRA PERMITIDA PRA INICIANTES)
             Console.Read();
