4881583a42595e6ba8503c7d5baa16360b4ef63f
Israel <rookie.qodeless@outlook.com>
2021-08-23 11:21:14 -0300

    Realizado o desafio 2

--- Program.cs
+++ Program.cs
@@ -30,18 +30,44 @@ namespace Desafio_02
 
 
             //T1 - Somar a idade de todos Ubuntus
+            int somaIdade = idade1 + idade2 + idade3;
 
             //T2 - Agrupar os nomes de todos Ubuntus
+            string groupNames = nome1 + ", " + nome2 + " e " + nome3;
 
             //T3 - Calcular Média dos IMC de todos Ubuntus
+            decimal pesoUbunto1 = Convert.ToDecimal(peso1);
+            decimal pesoUbunto2 = Convert.ToDecimal(peso2);
+            decimal pesoUbunto3 = Convert.ToDecimal(peso3);
+            decimal imcUbunto1 = pesoUbunto1 / (altura1 * altura1);
+            decimal imcUbunto2 = pesoUbunto2 / (altura2 * altura2);
+            decimal imcUbunto3 = pesoUbunto3 / (altura3 * altura3);
+
+            decimal media = (imcUbunto1 + imcUbunto2 + imcUbunto3) / 3;
 
             //T4 - Contar quantos Ubuntus sao Devs
+            int countDev = 0;
+
+            if (EhDev1 == true)
+                countDev += 1;
+            if (EhDev2 == true)
+                countDev += 1;
+            if (EhDev3 == true)
+                countDev += 1;
 
             //T5 - Exibir somente Ubuntu com nome Silva
+            string ubuntoSilva = null;
+            string word = "Silva";
 
-
+            if (nome1.Contains(word))
+                ubuntoSilva += nome1 + " e ";
+            if (nome2.Contains(word))
+                ubuntoSilva += nome2 + " e ";
+            if (nome3.Contains(word))
+                ubuntoSilva += nome3;
 
             //Imprime na tela
+            Console.WriteLine("Idade de todos os Ubuntos somadas: " + somaIdade + ";" + "\nNome de todos os Ubuntos: " + groupNames + ";" + "\nMédia do IMC de todos os Ubuntos: " + media + ";" + "\nQuantidade de Ubuntos que são Devs: " + countDev + ";" + "\nUbuntos que possuem o nome Silva: " + ubuntoSilva + ".");
             Console.WriteLine("UBUNTU 002 - ENDS...");
 
             //PAUSA NA TELA (GAMBIARRA PERMITIDA PRA INICIANTES)
